db2451a5f4ad4ca7239fe3287025df5993373bf5
AtulRanjan <atulranjan164@gmail.com>
2025-03-09 18:24:20 +0530

    add some

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
-const mongoose= require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-require('dotenv').config();
+const connectionDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "votingDB", // Specify your desired database name here
+    });
+    console.log("MongoDB is connected to votingDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+};
 
-const connectionDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDb is connected")
-    } catch (error) {
-        console.error("Mongo connection is failed " ,error);
-    }
-}
-module.exports = connectionDB
+module.exports = connectionDB;
